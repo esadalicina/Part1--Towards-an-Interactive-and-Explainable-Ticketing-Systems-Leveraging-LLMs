ab603d565a21a1de40a2fa973342819c06c320ee
esadalicina <61213644+esadalicina@users.noreply.github.com>
2024-05-09 20:48:01 +0200

    /

--- Source/Ticket-Submission/Website.py
+++ Source/Ticket-Submission/Website.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd
 from New_Ticket_Classification import predict_lr
-import random
 
 
 # Function to retrieve ticket solutions from the dataset
-def get_ticket_solutions(category, description, df):
+def get_ticket_solutions(category, df):
     if category:
         # Filter solutions based on category
         filtered_df = df[df["Topic_category"] == category]
@@ -15,10 +14,62 @@ def get_ticket_solutions(category, description, df):
         return df[["Topic_category", "Resolution", "complaint_what_happened"]].sample(n=5).values.tolist()  # Fetch 5 random solutions
 
 
-def main():
-    # Solution suggestions
-    st.sidebar.header("Solution Suggestions")
+# def main():
+#
+#     # Ticket submission form
+#     st.header("Ticket Submission")
+#
+#     df = pd.read_csv("/Users/esada/Documents/UNI.lu/MICS/Master-Thesis/Dataset/new_dataset.csv")
+#
+#     # Define category descriptions
+#     category_descriptions = {
+#         "Bank Account services": "Description for Bank Account services.",
+#         "Credit card or prepaid card": "Description for Credit card or prepaid card.",
+#         "Theft/Dispute Reporting": "Description for Theft/Dispute Reporting.",
+#         "Mortgage/Loan": "Description for Mortgage/Loan.",
+#         "Others": "Description for Other."
+#     }
+#
+#     # Initialize session state for selected_category
+#     if "selected_category" not in st.session_state:
+#         st.session_state.selected_category = None
+#
+#     # Display category descriptions when hovering over category
+#     category = st.selectbox("Category", [""] + list(category_descriptions.keys()), key="cat")
+#
+#     # Display category description based on selected category
+#     category_description = category_descriptions.get(category, "")
+#     st.write(f"<small>{category_description}</small>", unsafe_allow_html=True)
+#
+#     # Store selected category in session state
+#     st.session_state.selected_category = category
+#
+#     description = st.text_area("Description", placeholder="Please enter a detailed ticket description here.")
+#
+#     priority = st.radio("Priority", ["Low", "Medium", "High"])
+#
+#     if st.session_state.selected_category and description:
+#         # Classify the ticket
+#         predicted_category = predict_lr([description])
+#
+#         # If selected category differs from predicted category, ask for confirmation
+#         if st.session_state.selected_category != predicted_category:
+#             # Render radio buttons for category confirmation
+#             st.write("The program predicted that the category is:", predicted_category)
+#             st.write("Please check your ticket again.")
+#
+#             st.radio("Please select the correct category:", options=[st.session_state.selected_category, predicted_category])
+#
+#     # Conditionally set CSS style to disable the submit button
+#     button_disabled = not (st.session_state.selected_category and description)
+#
+#     if button_disabled:
+#         st.button("Submit", key="submit1", disabled=True)
+#     else:
+#         if st.button("Submit", key="submit2"):
+#             st.success("Ticket submitted successfully!")
 
+def main():
     # Ticket submission form
     st.header("Ticket Submission")
 
@@ -33,20 +84,14 @@ def main():
         "Others": "Description for Other."
     }
 
-    # Display category descriptions when hovering over category
-    category = st.selectbox("Category", [""] + list(category_descriptions.keys()))
-
-    # Initialize session state for selected_category
+    # Initialize session state for selected_category, description
     if "selected_category" not in st.session_state:
         st.session_state.selected_category = None
+    if "description" not in st.session_state:
+        st.session_state.description = ""
 
-    # Initialize session state for selected_category
-    if "category_user" not in st.session_state:
-        st.session_state.category_user = False
-
-    # Initialize session state for selected_category
-    if "category_pred" not in st.session_state:
-        st.session_state.category_pred = False
+    # Display category descriptions when hovering over category
+    category = st.selectbox("Category", [""] + list(category_descriptions.keys()), key="cat")
 
     # Display category description based on selected category
     category_description = category_descriptions.get(category, "")
@@ -55,23 +100,9 @@ def main():
     # Store selected category in session state
     st.session_state.selected_category = category
 
-    description = st.text_area("Description")
+    description = st.text_area("Description", value=st.session_state.description, placeholder="Please enter a detailed ticket description here.")
 
     priority = st.radio("Priority", ["Low", "Medium", "High"])
-
-    search_query = st.sidebar.text_input("Search Solutions")
-
-    # Retrieve ticket solutions from the dataset based on selected category and description
-    solutions = get_ticket_solutions(category, description, df)
-
-    if solutions:
-        for category, solution, complaint_what_happened in solutions:
-            if search_query.lower() in solution.lower() or search_query.lower() in complaint_what_happened.lower():
-                solution_partial = complaint_what_happened[:50] + "..." if len(solution) > 50 else solution
-                category_formatted = f"**{solution_partial}**"  # Format category name in bold
-                expander_title = f"{category_formatted}:"
-                with st.sidebar.expander(expander_title, expanded=False):
-                    st.write(solution)
 
     if st.session_state.selected_category and description:
         # Classify the ticket
@@ -83,29 +114,37 @@ def main():
             st.write("The program predicted that the category is:", predicted_category)
             st.write("Please check your ticket again.")
 
-            choice = st.radio("Please select the correct category:",
-                              options=[st.session_state.selected_category, predicted_category])
+            st.radio("Please select the correct category:", options=[st.session_state.selected_category, predicted_category])
 
     # Conditionally set CSS style to disable the submit button
     button_disabled = not (st.session_state.selected_category and description)
 
-    # Define button width and height
-    button_width = 75
-    button_height = 37
-
-    # Render the submit button with conditional styling
     if button_disabled:
-        st.markdown(
-            f'<button type="submit" style="pointer-events: none; border-radius: 5px; background-color: #ccc; border: 1px solid #999; width: {button_width}px; height: {button_height}px;">Submit</button>',
-            unsafe_allow_html=True
-        )
+        st.button("Submit", key="submit1", disabled=True)
     else:
-        if st.button("Submit"):
+        if st.button("Submit", key="submit2"):
             st.success("Ticket submitted successfully!")
-            # Clear input fields after submission
-            st.text_area("Description")
+            # Reset description and category selection after submission
+            st.session_state.description = ""
             st.session_state.selected_category = None
-            st.session_state.priority = None
+
+    # ------------------------------------------------ Solution suggestions --------------------------------------------
+
+    # Solution suggestions
+    st.sidebar.header("Solution Suggestions")
+    search_query = st.sidebar.text_input("Search Solutions")
+
+    # Retrieve ticket solutions from the dataset based on selected category and description
+    solutions = get_ticket_solutions(category, df)
+
+    if solutions:
+        for category, solution, complaint_what_happened in solutions:
+            if search_query.lower() in solution.lower() or search_query.lower() in complaint_what_happened.lower():
+                solution_partial = complaint_what_happened[:50] + "..." if len(solution) > 50 else solution
+                category_formatted = f"**{solution_partial}**"  # Format category name in bold
+                expander_title = f"{category_formatted}:"
+                with st.sidebar.expander(expander_title, expanded=False):
+                    st.write(solution)
 
 
 if __name__ == "__main__":
